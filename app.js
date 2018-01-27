01f95b8c357986a5c4bc35d709e621d6c8b33f92
shrit2010 <shrit2010@gmail.com>
2018-01-27 07:40:09 +0530

    First commit on github

---
+++ app.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+    angular.module("myapp", [])
+        .controller('myController', myController)
+    .filter('first', FirstFilterFactory);
+    myController.$inject = ['$scope', 'firstFilter'];
+    function myController($scope,firstFilter)
+    {
+        var msg = "hello";
+
+        $scope.testFilter = function ()
+        {
+           return firstFilter(msg);
+        }
+    }
+
+    function FirstFilterFactory() {
+
+        return function (input) {
+
+            return input + "you";
+
+        }
+
+    }
+})();
